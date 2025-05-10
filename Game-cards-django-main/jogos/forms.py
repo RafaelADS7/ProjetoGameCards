beaaa336984c0338214b4e5c72f68192ae21d4c0
Rafael Barbosa Ramos <141770380+RafaelADS7@users.noreply.github.com>
2025-05-10 17:16:01 -0300

    Delete Game-cards-django-main directory

--- Game-cards-django-main/jogos/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Jogo
-
-class JogoForm(forms.ModelForm):
-    class Meta:
-        model = Jogo
-        fields = ['titulo', 'estudio', 'data_lancamento', 'descricao', 'capa', 'background']
